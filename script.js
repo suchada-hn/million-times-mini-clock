158b8ee07fec11d9620cd5a973867d4011dac290
Suchada Hnoonpakdee <53847107+suchada-hn@users.noreply.github.com>
2025-02-20 09:58:33 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,46 +1,19 @@
-window.addEventListener("DOMContentLoaded", () => {
-	// Set odometers
+const hr1 = document.getElementById('hr-1');
+const hr2 = document.getElementById('hr-2');
+const mn1 = document.getElementById('mn-1');
+const mn2 = document.getElementById('mn-2');
 
-	const elements = [...document.querySelectorAll(".progress-bar__steps-title")];
-
-	const steps = [6784, 3264, 4532];
-	const odometers = [];
-
-	elements.forEach((element, index) => {
-		odometers.push({
-			instance: new Odometer({ el: element, format: "(,ddd)", duration: 250 }),
-			value: steps[index]
-		});
-	});
-
-	for (let i = 0; i < odometers.length; i++) {
-		setTimeout(() => {
-			odometers[i].instance.update(odometers[i].value);
-		}, i * 250);
-	}
-
-	// Update time
-
-	const clocks = [...document.querySelectorAll("time")];
-
-	const update = () => {
-		const now = new Date();
-
-		const hours = now.getHours().toString().padStart(2, "0");
-		const minutes = now.getMinutes().toString().padStart(2, "0");
-		const datetime = `${hours}:${minutes}`;
-
-		const outputHours = now.getHours() % 12 || 12;
-		const outputAMPM = now.getHours() >= 12 ? "PM" : "AM";
-		const output = `${outputHours}:${minutes.padStart(2, "0")} ${outputAMPM}`;
-
-		clocks.forEach((clock) => {
-			clock.setAttribute("datetime", datetime);
-			clock.textContent = output;
-		});
-	};
-
-	update();
-
-	setInterval(update, 60000);
-});
+setInterval(()=> {
+  const dd = new Date();
+  const HH = dd.getHours();
+  const hh = HH < 13? HH: HH - 12;
+  const h1 = hh > 9 && hh < 13 ? 1: 0;
+  const h2 = hh < 10? hh: hh.toString()[1];
+  const mm = dd.getMinutes();
+  const m1 = mm < 10? 0: mm.toString()[0];
+  const m2 = mm < 10? mm: mm.toString()[1];
+  mn1.setAttribute('data-digit', m1);
+  mn2.setAttribute('data-digit', m2);
+  hr1.setAttribute('data-digit', h1);
+  hr2.setAttribute('data-digit', h2);
+}, 1000)
